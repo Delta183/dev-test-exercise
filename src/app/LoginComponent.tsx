55042c5bd18d4175181e0130ca7333b3aa53da30
Daniel Figueroa <danfigueroa81@gmail.com>
2023-06-20 12:36:57 -0400

    add validator for emails

--- src/app/LoginComponent.tsx
+++ src/app/LoginComponent.tsx
@@ -3,48 +3,71 @@ import React, { useState }  from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
+import validator from 'validator'
 
 const initialState = { email: '', password: ''};
 
 const LoginComponent = () => {
     const [formData, setFormData] = useState(initialState);
     const [showPassword, setShowPassword] = useState(false);
+    const [emailError, setEmailError] = useState('')
+
     // This will toggle the state of the visibility of the password
     const handleShowPassword = () => setShowPassword(!showPassword);
     const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => setFormData({ ...formData, [e.target.name]: e.target.value });
   
-    return (
-    <div>
-    <Form autoComplete="off">
-        {/* First Segment */}
-        <Form.Group className="m-3" >
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-            name="email"
-            type="email" 
-            placeholder="Enter your email..." 
-            onChange={handleChange}
-            />
-        </Form.Group>
+    const validateEmail = (e : React.ChangeEvent<HTMLInputElement>) => {
+        var email = e.target.value
+      
+        if (validator.isEmail(email)) {
+           setEmailError('Valid Email :)')
+        } else {
+           setEmailError('Enter valid Email!')
+        }
+      }
     
-        {/* Second Segment */}
-        <Form.Group className="m-3">
-            <Form.Label>Password</Form.Label>
-            <InputGroup>
+    const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+      }
+
+    return (
+    <div className="container">
+        <h1>Log In</h1>
+        <Form autoComplete="off">
+            {/* First Segment */}
+            <Form.Group className="m-3" >
+                <Form.Label>Email</Form.Label>
                 <Form.Control
-                name="password"
-                type={showPassword ? 'text' : 'password'}
-                placeholder="Enter your password..." 
-                onChange={handleChange}
+                name="email"
+                type="email" 
+                placeholder="Enter your email..." 
+                onChange={validateEmail}
                 />
-                <Button onClick={handleShowPassword} variant="outline-secondary" id="button-addon2">
-                    Show Password
-                </Button>
-            </InputGroup>
-        </Form.Group>
-    </Form>
-   </div>
-    )
+               
+            </Form.Group>
+        
+            {/* Second Segment */}
+            <Form.Group className="m-3">
+                <Form.Label>Password</Form.Label>
+                <InputGroup>
+                    <Form.Control
+                    name="password"
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Enter your password..." 
+                    onChange={handleChange}
+                    />
+                    <Button onClick={handleShowPassword} variant="outline-secondary" id="button-addon2">
+                        Show Password
+                    </Button>
+                </InputGroup>
+            </Form.Group>
+            <Button className="m-3" variant="primary" type="submit">
+                Log In
+        </Button>
+        </Form>
+    </div>
+    )   
 }
+
 
 export default LoginComponent;
